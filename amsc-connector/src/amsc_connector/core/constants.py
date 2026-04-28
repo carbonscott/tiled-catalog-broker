47e4bb88076d21ef03d535ec64464d8b100e9659
Sam Welborn <swelborn@slac.stanford.edu>
2026-04-28 16:33:16 -0400

    [#52] push to amsc catalog
    here we push our data to amsc using faststream + exception handling
    
    when we receive an event, which in this case just contains the path to the tiled URI, we ask tiled for the most up to date information about this when we receive the event. this prevents fruststration with out of order-ness of events, and just gets the most up to date information whether ot not this is a metadata event or child created event
     
    this requires an extra call or two to tiled but it is worth it to avoid statefullness headaches

--- amsc-connector/src/amsc_connector/core/constants.py
+++ amsc-connector/src/amsc_connector/core/constants.py
@@ -1,11 +1,15 @@
 from tiled.server.schemas import EventType
 
 STREAM_PREFIX = "amsc:events"
+STREAM_SYNC = f"{STREAM_PREFIX}:sync"
+STREAM_CLOSED = f"{STREAM_PREFIX}:stream_closed"
 STREAM_DLQ = f"{STREAM_PREFIX}:dlq"
 SEEN_KEY_PREFIX = "amsc:seen"
 SEEN_TTL_SECONDS = 3600
 HEADER_EVENT_ID = "x-tiled-event-id"
 
 EVENT_TYPE_TO_STREAM: dict[EventType, str] = {
-    e: f"{STREAM_PREFIX}:{e.value}" for e in EventType
+    EventType.container_child_created: STREAM_SYNC,
+    EventType.container_child_metadata_updated: STREAM_SYNC,
+    EventType.stream_closed: STREAM_CLOSED,
 }
